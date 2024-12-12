c9c72898d8ad1cb99f13857d3295a2042c032af0
RaceFPV <racefpv@outlook.com>
2024-12-11 22:36:35 -0500

    disable the way the b key brings up the build menu for now

--- frontend/src/app/module-blueprint/directives/keyboard.directive.ts
+++ frontend/src/app/module-blueprint/directives/keyboard.directive.ts
@@ -1,10 +1,30 @@
-import { Directive, Output, HostListener, EventEmitter } from "@angular/core";
+import { Directive, HostListener } from "@angular/core";
+import { ToolService } from "../services/tool-service";
+import { ToolType } from "../common/tools/tool";
 
-@Directive({ selector: "[appKeyEvents]" })
+@Directive({
+  selector: "[appKeyboard]",
+})
 export class KeyboardDirective {
-  @Output() keyPress = new EventEmitter();
+  constructor(private toolService: ToolService) {}
 
-  @HostListener("document:keydown", ["$event"]) onKeyPress(event: any) {
-    this.keyPress.emit(event);
+  @HostListener("document:keydown", ["$event"])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // Ignore keyboard shortcuts if target is an input or if a dialog is open
+    if (
+      event.target instanceof HTMLInputElement ||
+      event.target instanceof HTMLTextAreaElement ||
+      document.querySelector(".p-dialog-visible")
+    ) {
+      // Check for visible PrimeNG dialog
+      return;
+    }
+
+    switch (event.key.toLowerCase()) {
+      case "b":
+        this.toolService.changeTool(ToolType.build);
+        break;
+      // ... rest of your keyboard shortcuts
+    }
   }
 }
